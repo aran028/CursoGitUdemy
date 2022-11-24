cceaa3b9b1629d10ec13b86404a2d77f2a6a8d98
Aranzazu Foronda <afa028@gmail.com>
2022-11-24 12:22:53 +0100

    Commit que será borrado

--- Inicio.java
+++ Inicio.java
@@ -3,6 +3,7 @@ public class Inicio{
 public static void main(String [] args){
 System.out.println("Bienvenidos");
 System.out.println("Aprendiendo git cherry-pick");
+System.out.println("Linea a eliminar del commit");
 }
 
 }
