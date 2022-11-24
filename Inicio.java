ab8c07c92ea3ea6987909161372b1e3d0714ca26
Aranzazu Foronda <afa028@gmail.com>
2022-11-24 12:38:41 +0100

    Eliminando commit

--- Inicio.java
+++ Inicio.java
@@ -4,6 +4,7 @@ public static void main(String [] args){
 System.out.println("Bienvenidos");
 System.out.println("Aprendiendo git cherry-pick");
 System.out.println("Linea a eliminar del commit");
+System.out.println("Otra Linea a eliminar del commit");
 }
 
 }
