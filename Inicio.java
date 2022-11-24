64d05a1decf50788664280e42e265ac67bfb3659
Aranzazu Foronda <afa028@gmail.com>
2022-11-24 12:51:57 +0100

    Commit que luego borrare

--- Inicio.java
+++ Inicio.java
@@ -5,6 +5,7 @@ System.out.println("Bienvenidos");
 System.out.println("Aprendiendo git cherry-pick");
 System.out.println("Linea a eliminar del commit");
 System.out.println("Otra Linea a eliminar del commit");
+System.out.println("Otra prueba on eliminar commit");
 }
 
 }
